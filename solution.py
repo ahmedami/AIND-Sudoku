03b5ccce2b3e1f1c0261d1577088dbd8e87c5f46
Ahmed Abdelrahman <eng.ahmedami@gmail.com>
2017-05-29 23:37:53 +0200

    finish sudoku AI agent

--- solution.py
+++ solution.py
@@ -56,52 +56,28 @@ def naked_twins(values):
         return False
     #iterate over all the unites
     for unit in unitlist:
-        #iterate overall the boxes in each unit first pointer
+        #save the boxes with lenght two for the current unit
+        tower_boxes={}
+        #save the twins keys for the current unit, key is the first box , value is the second box
+        twin_boxes = {}
+        #iterate overall the boxes in the current unit
         for box in unit:
-            # get the first pointer value
-            value = values[box]
-            #ccontinue if it's more than 2 character
-            if len(value) != 2:
-                continue
-            #iterate over all boxes in the unit with second pointer
-            for box2 in unit:
-                # continue if it point to itself .
-                if box == box2:
-                    continue
-                # get the second pointer value
-                value2 = values[box2]
-                # check naked twin condition
-                is_naked_twin = value==value2 or value[1]+value[0]==value2
-                if is_naked_twin and len(value2)==2:
-                    #iterate over all boxes in the same unit with a third pointer
-                    for box3 in unit:
-                        #continue if it point to one of the naked twin pointer
-                        if box3 == box2 or box3 == box:
-                            continue
-                        # iterate over each charachter in naked twin value
-                        for c in value:
-                            # remove that charachter from any box in the unit
-                            if len(values[box3]) > 1 and c in values[box3]:
-                                values[box3]=values[box3].replace(c,"")
-                # check hidden twin
-                elif value in value2 and len(value2) > 2:
-                    # create flag to detect hidden twin
-                    hidden_twin = True
-                    for box3 in unit:
-                        if not hidden_twin:
-                            break
-                        # continue if it point to one of the naked twin pointer
-                        if box3 == box2 or box3 == box :
-                            continue
-                        # iterate over each charachter in naked twin value
-                        for c in value:
-                            # remove that character from any box in the unit
-                            if c in values[box3]:
-                                hidden_twin = False
-                                break
-                    if hidden_twin:
-                        values[box2] = value
-
+            # get all boxes with lenght two
+            if len(values[box]) == 2:
+                tower_boxes[box]=values[box]
+        #detect twins
+        for key, val in tower_boxes.items():
+            for key1, val1 in tower_boxes.items():
+                is_twin = val1==val or val1 == val[1]+val[0]
+                if key1 != key and is_twin:
+                    twin_boxes[key]=key1
+        # iterate over the unit to remove the twin value from other boxes
+        for key, key1 in twin_boxes.items():
+            for box in unit:
+                #make sure that we didn't remove the twin value itself
+                if box != key and box != key1:
+                    for c in values[key]:
+                        values[box]=values[box].replace(c,"")
     #sort the values by the key to pass the test case
     values = dict(sorted(values.items(), key=operator.itemgetter(0)))
     time_taken = int(round(time.time() * 1000))-start_time
@@ -222,25 +198,8 @@ def solve(grid):
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    #display(solve(diag_sudoku_grid))
-    display(naked_twins({"G7": "1234568", "G6": "9", "G5": "35678", "G4": "23678", "G3":
-"245678", "G2": "123568", "G1": "1234678", "G9": "12345678", "G8":
-"1234567", "C9": "13456", "C8": "13456", "C3": "4678", "C2": "68",
-"C1": "4678", "C7": "13456", "C6": "368", "C5": "2", "A4": "5", "A9":
-"2346", "A8": "2346", "F1": "123689", "F2": "7", "F3": "25689", "F4":
-"23468", "F5": "1345689", "F6": "23568", "F7": "1234568", "F8":
-"1234569", "F9": "1234568", "B4": "46", "B5": "46", "B6": "1", "B7":
-"7", "E9": "12345678", "B1": "5", "B2": "2", "B3": "3", "C4": "9",
-"B8": "8", "B9": "9", "I9": "1235678", "I8": "123567", "I1": "123678",
-"I3": "25678", "I2": "123568", "I5": "35678", "I4": "23678", "I7":
-"9", "I6": "4", "A1": "2468", "A3": "1", "A2": "9", "A5": "3468",
-"E8": "12345679", "A7": "2346", "A6": "7", "E5": "13456789", "E4":
-"234678", "E7": "1234568", "E6": "23568", "E1": "123689", "E3":
-"25689", "E2": "123568", "H8": "234567", "H9": "2345678", "H2":
-"23568", "H3": "2456789", "H1": "2346789", "H6": "23568", "H7":
-"234568", "H4": "1", "H5": "35678", "D8": "1235679", "D9": "1235678",
-"D6": "23568", "D7": "123568", "D4": "23678", "D5": "1356789", "D2":
-"4", "D3": "25689", "D1": "123689"}))
+    display(solve(diag_sudoku_grid))
+
     try:
         from visualize import visualize_assignments
         visualize_assignments(assignments)
