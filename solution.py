3cbe109e79209ec2f63c0360fb417e6245c9cd38
Ahmed Abdelrahman <eng.ahmedami@gmail.com>
2017-05-29 22:09:47 +0200

    add new condition

--- solution.py
+++ solution.py
@@ -2,16 +2,19 @@ import operator
 import logging
 import time
 assignments = []
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
 rows = 'ABCDEFGHI'
 cols = '123456789'
-boxes = []
-row_units = []
-column_units = []
-square_units = []
-diagonal_units=[]
-unitlist = []
-units = {}
-peers = {}
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+diagonal_units = [[r + c for r, c in zip(rows, cols)], [r + c for r, c in zip(rows, cols[::-1])]]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
 def assign_value(values, box, value):
     """
@@ -68,7 +71,8 @@ def naked_twins(values):
                 # get the second pointer value
                 value2 = values[box2]
                 # check naked twin condition
-                if value==value2 and len(value2)==2:
+                is_naked_twin = value==value2 or value[1]+value[0]==value2
+                if is_naked_twin and len(value2)==2:
                     #iterate over all boxes in the same unit with a third pointer
                     for box3 in unit:
                         #continue if it point to one of the naked twin pointer
@@ -103,10 +107,6 @@ def naked_twins(values):
     time_taken = int(round(time.time() * 1000))-start_time
     logging.info('Finish naked twins in: '+str(time_taken)+ ' millisec')
     return values
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    return [a+b for a in A for b in B]
 
 def grid_values(grid):
     """
@@ -214,22 +214,6 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
-    global boxes
-    boxes = cross(rows, cols)
-    global row_units
-    row_units = [cross(r, cols) for r in rows]
-    global column_units
-    column_units = [cross(rows, c) for c in cols]
-    global square_units
-    square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-    global diagonal_units
-    diagonal_units = [[r + c for r, c in zip(rows, cols)], [r + c for r, c in zip(rows, cols[::-1])]]
-    global unitlist
-    unitlist = row_units + column_units + square_units + diagonal_units
-    global units
-    units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-    global peers
-    peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
     values=grid_values(grid)
     values=search(values)
     return values
@@ -238,8 +222,25 @@ def solve(grid):
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(solve(diag_sudoku_grid))
-
+    #display(solve(diag_sudoku_grid))
+    display(naked_twins({"G7": "1234568", "G6": "9", "G5": "35678", "G4": "23678", "G3":
+"245678", "G2": "123568", "G1": "1234678", "G9": "12345678", "G8":
+"1234567", "C9": "13456", "C8": "13456", "C3": "4678", "C2": "68",
+"C1": "4678", "C7": "13456", "C6": "368", "C5": "2", "A4": "5", "A9":
+"2346", "A8": "2346", "F1": "123689", "F2": "7", "F3": "25689", "F4":
+"23468", "F5": "1345689", "F6": "23568", "F7": "1234568", "F8":
+"1234569", "F9": "1234568", "B4": "46", "B5": "46", "B6": "1", "B7":
+"7", "E9": "12345678", "B1": "5", "B2": "2", "B3": "3", "C4": "9",
+"B8": "8", "B9": "9", "I9": "1235678", "I8": "123567", "I1": "123678",
+"I3": "25678", "I2": "123568", "I5": "35678", "I4": "23678", "I7":
+"9", "I6": "4", "A1": "2468", "A3": "1", "A2": "9", "A5": "3468",
+"E8": "12345679", "A7": "2346", "A6": "7", "E5": "13456789", "E4":
+"234678", "E7": "1234568", "E6": "23568", "E1": "123689", "E3":
+"25689", "E2": "123568", "H8": "234567", "H9": "2345678", "H2":
+"23568", "H3": "2456789", "H1": "2346789", "H6": "23568", "H7":
+"234568", "H4": "1", "H5": "35678", "D8": "1235679", "D9": "1235678",
+"D6": "23568", "D7": "123568", "D4": "23678", "D5": "1356789", "D2":
+"4", "D3": "25689", "D1": "123689"}))
     try:
         from visualize import visualize_assignments
         visualize_assignments(assignments)
